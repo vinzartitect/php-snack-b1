105fed92969d2719cda78250ed31a03a60a1baaf
VincenzoBorriello89 <vincborr89@gmail.com>
2022-05-05 16:34:07 +0200

    file rinominati e creaz snack 7

--- snack4/index.php
+++ snack4/index.php
@@ -1,4 +1,4 @@
-<!-- ## Snack 3
+<!-- ## Snack 4
 
 Creare un array con 15 numeri casuali, 
 tenendo conto che l'array non dovrà contenere lo stesso numero più di una volta
@@ -11,7 +11,7 @@ tenendo conto che l'array non dovrà contenere lo stesso numero più di una volt
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Snack3</title>
+    <title>Snack4</title>
 </head>
 <body>
 
---
+++ snack7/index.php
@@ -0,0 +1,26 @@
+<!-- ## Snack 7
+
+Creare un array contenente qualche alunno di un'ipotetica classe. Ogni alunno avrà Nome, 
+Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno.
+    
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack7</title>
+</head>
+<body>
+
+<h2>Snack 7</h2>
+
+<?php
+
+
+?>
+
+</body>
+</html>
